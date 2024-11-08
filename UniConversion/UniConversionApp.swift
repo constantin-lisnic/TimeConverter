afea878d9a8856de60af8e9244c868b61b3df302
Constantin Lisnic <constantin@lisnic.dev>
2024-11-08 17:07:00 +0000

    Initial Commit

---
+++ UniConversion/UniConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniConversionApp.swift
+//  UniConversion
+//
+//  Created by Constantin Lisnic on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UniConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniConversionTests/UniConversionTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniConversionTests.swift
+//  UniConversionTests
+//
+//  Created by Constantin Lisnic on 08/11/2024.
+//
+
+import Testing
+@testable import UniConversion
+
+struct UniConversionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
